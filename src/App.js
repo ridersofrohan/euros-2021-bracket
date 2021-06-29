f3537b97057e41d2d656708a6f4d48ee05dfa7fb
ridersofrohan <rchalla769@gmail.com>
2021-06-29 13:45:16 -0400

    england wins

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
   const matches = [
     [
       [{h: "BEL", a: "POR", w: "BEL"}, {h: "ITA", a: "AUT", w: "ITA"}, {h: "FRA", a: "SUI", w: "SUI"}, {h: "CRO", a: "ESP", w: "ESP"}],
-      [{h: "SWE", a: "UKR", w: ""}, {h: "ENG", a: "GER", w: ""}, {h: "NED", a: "CZE", w: "CZE"}, {h: "WAL", a: "DEN", w: "DEN"}],
+      [{h: "SWE", a: "UKR", w: ""}, {h: "ENG", a: "GER", w: "ENG"}, {h: "NED", a: "CZE", w: "CZE"}, {h: "WAL", a: "DEN", w: "DEN"}],
     ],
     [
       [{h: "BEL", a: "ITA", w: ""}, {h: "SUI", a: "ESP", w: ""}],
-      [{h: "", a: "", w: ""}, {h: "CZE", a: "DEN", w: ""}],
+      [{h: "", a: "ENG", w: ""}, {h: "CZE", a: "DEN", w: ""}],
     ],
     [
       [{h: "", a: "", w: ""}],
